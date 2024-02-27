1b4d0c003cc0b20d715362f40c474c42e3f36639
ITSTEP\st <st>
2024-02-27 17:45:30 +0300

    Add project files.

---
+++ Project01/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int size = 5;
+
+	//do {
+	//	cout << "Input size of array: ";
+	//	cin >> size;
+	//} while (size <= 0);
+
+	int* pointer = new int[size];
+
+	//pointer[0] = 10;
+	//pointer[1] = 10;
+	//pointer[2] = 10;
+	//pointer[3] = 10;
+	//pointer[4] = 10;
+
+	//cout << pointer[0] << endl;
+	//cout << pointer[1] << endl;
+	//cout << pointer[2] << endl;
+	//cout << pointer[3] << endl;
+	//cout << pointer[4] << endl;
+
+	*(pointer + 0) = 11;
+	*(pointer + 1) = 22;
+	*(pointer + 2) = 33;
+	*(pointer + 3) = 44;
+	*(pointer + 4) = 55;
+
+	cout << pointer + 0 << " - " << *(pointer + 0) << endl;
+	cout << pointer + 1 << " - " << *(pointer + 1) << endl;
+	cout << pointer + 2 << " - " << *(pointer + 2) << endl;
+	cout << pointer + 3 << " - " << *(pointer + 3) << endl;
+	cout << pointer + 4 << " - " << *(pointer + 4) << endl;
+
+	return 0;
+}
