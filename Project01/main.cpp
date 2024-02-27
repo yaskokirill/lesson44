a2f87068beb3243653b2babc4a4024121b895f74
ITSTEP\st <st>
2024-02-27 17:50:59 +0300

    test01 finished

--- Project01/main.cpp
+++ Project01/main.cpp
@@ -3,14 +3,26 @@
 using namespace std;
 
 int main() {
-	int size = 5;
+	srand(time(NULL));
+	int size;
 
-	//do {
-	//	cout << "Input size of array: ";
-	//	cin >> size;
-	//} while (size <= 0);
+	do {
+		cout << "Input size of array: ";
+		cin >> size;
+	} while (size <= 0);
 
 	int* pointer = new int[size];
+
+	for (int i = 0; i < size; i++)
+	{
+		*(pointer + i) = rand() % 10;
+	}
+
+	cout << "\nelements of dynamic array: ";
+	for (int i = 0; i < size; i++)
+	{
+		cout << *(pointer + i) << " ";
+	}
 
 	//pointer[0] = 10;
 	//pointer[1] = 10;
@@ -24,17 +36,17 @@ int main() {
 	//cout << pointer[3] << endl;
 	//cout << pointer[4] << endl;
 
-	*(pointer + 0) = 11;
-	*(pointer + 1) = 22;
-	*(pointer + 2) = 33;
-	*(pointer + 3) = 44;
-	*(pointer + 4) = 55;
+	//*(pointer + 0) = 11;
+	//*(pointer + 1) = 22;
+	//*(pointer + 2) = 33;
+	//*(pointer + 3) = 44;
+	//*(pointer + 4) = 55;
 
-	cout << pointer + 0 << " - " << *(pointer + 0) << endl;
-	cout << pointer + 1 << " - " << *(pointer + 1) << endl;
-	cout << pointer + 2 << " - " << *(pointer + 2) << endl;
-	cout << pointer + 3 << " - " << *(pointer + 3) << endl;
-	cout << pointer + 4 << " - " << *(pointer + 4) << endl;
+	//cout << pointer + 0 << " - " << *(pointer + 0) << endl;
+	//cout << pointer + 1 << " - " << *(pointer + 1) << endl;
+	//cout << pointer + 2 << " - " << *(pointer + 2) << endl;
+	//cout << pointer + 3 << " - " << *(pointer + 3) << endl;
+	//cout << pointer + 4 << " - " << *(pointer + 4) << endl;
 
 	return 0;
 }
